abbd3f3e00f147b8d0e348fed3b75e63845e70eb
C2dan88 <wildteen88@googlemail.com>
2019-08-25 09:04:51 +0100

    Added LoadFont helper function

--- text.h
+++ text.h
@@ -20,14 +20,6 @@
 #define TTF_FONT_PATH(a) concat(2, FONT_BASEDIR, a)
 
 
-// char* concat(const char *s1, const char *s2)
-// {
-//     char *result = malloc(strlen(s1) + strlen(s2) + 1);
-//     strcpy(result, s1);
-//     strcat(result, s2);
-//     return result;
-// }
-
 char* concat(int howMany, ...)
 {
     va_list strP;
